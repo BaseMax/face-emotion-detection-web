c7f9bd62e944db74633b6ec8b7b931e1e42fc1ff
Max Base <MaxBaseCode@Gmail.Com>
2024-12-24 01:16:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ async function loadModels() {
     await faceapi.nets.ssdMobilenetv1.loadFromUri('models');
     await faceapi.nets.faceLandmark68Net.loadFromUri('models');
     await faceapi.nets.faceRecognitionNet.loadFromUri('models');
-    // await faceapi.nets.ageGenderNet.loadFromUri('models');
+    await faceapi.nets.ageGenderNet.loadFromUri('models');
     await faceapi.nets.faceExpressionNet.loadFromUri('models');
+    
     console.log("Models loaded successfully.");
 
     document.getElementById('imageUpload').disabled = false;
